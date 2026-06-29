5006b45fc770327b4d550708a524b1508f6f0ba2
edisqc3-design <edisqc3@gmail.com>
2026-06-30 08:00:01 +0900

    Add files via upload

--- src/app/inquiry/board/page.tsx
+++ src/app/inquiry/board/page.tsx
@@ -1,1 +1,9 @@
+import { getAllPublicInquiries } from '@/lib/queries'
+import InquiryBoardClient from './InquiryBoardClient'
 
+export const revalidate = 60
+
+export default async function InquiryBoardPage() {
+  const inquiries = await getAllPublicInquiries()
+  return <InquiryBoardClient inquiries={inquiries} />
+}
